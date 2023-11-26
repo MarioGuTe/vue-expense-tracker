eac1b388c17649e6f2626d645eaf31106d72467d
MarioGuTe <mariogutierreztello@gmail.com>
2023-11-26 03:26:55 -0300

    add props and logic

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+      :key="transaction.id"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
