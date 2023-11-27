188af4b58855b28904b4b7e0f83ce9f32bec3317
MarioGuTe <mariogutierreztello@gmail.com>
2023-11-27 02:06:31 -0300

    add functions to get total income and total expenses and pass props

--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -2,11 +2,26 @@
   <div class="inc-exp-container">
     <div>
       <h4>Income</h4>
-      <p id="money-plus" class="money plus">+100</p>
+      <p id="money-plus" class="money plus">+${{ income }}</p>
     </div>
     <div>
       <h4>Expense</h4>
-      <p id="money-minus" class="money minus">-$100</p>
+      <p id="money-minus" class="money minus">-${{ `${expenses.slice(1)}` }}</p>
     </div>
   </div>
 </template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  income: {
+    type: Number,
+    required: true,
+  },
+  expenses: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
