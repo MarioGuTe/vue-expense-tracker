770ca0bb1143f56300aa7d5b424d9628874e6d13
MarioGuTe <mariogutierreztello@gmail.com>
2023-11-28 01:50:50 -0300

    add toastification logic for error handling onSubmit

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,17 +1,46 @@
 <template>
   <h3>Add new transaction</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" placeholder="Enter text..." />
+      <input type="text" id="text" v-model="text" placeholder="Enter text..." />
     </div>
     <div class="form-control">
       <label for="amount"
         >Amount <br />
         (negative - expense, positive - income)</label
       >
-      <input type="text" id="amount" placeholder="Enter amount..." />
+      <input
+        type="text"
+        id="amount"
+        v-model="amount"
+        placeholder="Enter amount..."
+      />
     </div>
     <button class="btn">Add transaction</button>
   </form>
 </template>
+
+<script setup>
+import { useToast } from "vue-toastification";
+import { ref } from "vue";
+
+const text = ref("");
+const amount = ref("");
+
+// Get toast interface
+const toast = useToast();
+
+const onSubmit = () => {
+  if (!text.value || !amount.value) {
+    // Display a toast error message if either field is empty
+    toast.error("Both fields must be filled.");
+    return;
+  }
+
+  console.log(text.value, amount.value);
+  // Clear form fields
+  text.value = "";
+  amount.value = "";
+};
+</script>
